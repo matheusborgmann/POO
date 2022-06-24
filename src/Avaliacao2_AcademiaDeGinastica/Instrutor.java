6dbc07110a4da94f625eed63ceef230055990480
matheusborgmann <matheusestevanborg@gmail.com>
2022-06-23 23:59:26 -0300

    aaa

--- src/Avaliacao2_AcademiaDeGinastica/Instrutor.java
+++ src/Avaliacao2_AcademiaDeGinastica/Instrutor.java
@@ -1,4 +1,61 @@
 package Avaliacao2_AcademiaDeGinastica;
 
 public class Instrutor {
+
+    private String nome;
+    private String rg;
+    private String dataNasc;
+    private String titu;
+    private String tel;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getTitu() {
+        return titu;
+    }
+
+    public void setTitu(String titu) {
+        this.titu = titu;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    @Override
+    public String toString() {
+        return "Instrutor{" +
+                "nome='" + nome + '\'' +
+                ", rg=" + rg +
+                ", dataNasc='" + dataNasc + '\'' +
+                ", titu='" + titu + '\'' +
+                ", tel='" + tel + '\'' +
+                '}';
+    }
 }
--- src/Avaliacao2_AcademiaDeGinastica/Turma.java
+++ src/Avaliacao2_AcademiaDeGinastica/Turma.java
@@ -1,14 +1,50 @@
 package Avaliacao2_AcademiaDeGinastica;
 
+import java.util.Collection;
 import java.util.Date;
 
 public class Turma {
+
+
+    protected String nome;
+
+    protected Aluno aluno;
     protected int numAlunos;
-    protected float horarioDeAula;
-    protected int duracaoDeAula;
-    protected Date dataInicial, dataFinal;
+    protected String horarioDeAula;
+    protected String duracaoDeAula;
+    protected String dataInicial, dataFinal;
     protected String tipoDeAtividade;
     protected Instrutor instrutor;
+
+    public Turma(String nome, int numAlunos, String horarioDeAula, String duracaoDeAula, String dataInicial, String dataFinal, String tipoDeAtividade) {
+        this.nome = nome;
+        this.numAlunos = numAlunos;
+        this.horarioDeAula = horarioDeAula;
+        this.duracaoDeAula = duracaoDeAula;
+        this.dataInicial = dataInicial;
+        this.dataFinal = dataFinal;
+        this.tipoDeAtividade = tipoDeAtividade;
+    }
+
+    public Turma(Instrutor instrutor) {
+        this.instrutor = instrutor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
 
     public int getNumAlunos() {
         return numAlunos;
@@ -18,35 +54,35 @@ public class Turma {
         this.numAlunos = numAlunos;
     }
 
-    public float getHorarioDeAula() {
+    public String getHorarioDeAula() {
         return horarioDeAula;
     }
 
-    public void setHorarioDeAula(float horarioDeAula) {
+    public void setHorarioDeAula(String horarioDeAula) {
         this.horarioDeAula = horarioDeAula;
     }
 
-    public int getDuracaoDeAula() {
+    public String getDuracaoDeAula() {
         return duracaoDeAula;
     }
 
-    public void setDuracaoDeAula(int duracaoDeAula) {
+    public void setDuracaoDeAula(String duracaoDeAula) {
         this.duracaoDeAula = duracaoDeAula;
     }
 
-    public Date getDataInicial() {
+    public String getDataInicial() {
         return dataInicial;
     }
 
-    public void setDataInicial(Date dataInicial) {
+    public void setDataInicial(String dataInicial) {
         this.dataInicial = dataInicial;
     }
 
-    public Date getDataFinal() {
+    public String getDataFinal() {
         return dataFinal;
     }
 
-    public void setDataFinal(Date dataFinal) {
+    public void setDataFinal(String dataFinal) {
         this.dataFinal = dataFinal;
     }
 
@@ -64,5 +100,22 @@ public class Turma {
 
     public void setInstrutor(Instrutor instrutor) {
         this.instrutor = instrutor;
+    }
+
+    @Override
+    public String toString() {
+        return "\nTurma{" +
+                "nome='" + nome + '\'' +
+/*
+                ", aluno=" + aluno +
+*/
+                ", numAlunos=" + numAlunos +
+                ", horarioDeAula=" + horarioDeAula +
+                ", duracaoDeAula=" + duracaoDeAula +
+                ", dataInicial=" + dataInicial +
+                ", dataFinal=" + dataFinal +
+                ", tipoDeAtividade='" + tipoDeAtividade + '\'' +
+                ", instrutor=" + instrutor +
+                '}';
     }
 }
